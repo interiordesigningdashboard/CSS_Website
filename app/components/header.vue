665647381b3900984d88754cba44ca57b0416ba3
vk <vishnukarthik2912@gmail.com>
2026-02-26 12:27:11 +0530

    updated the header

--- app/components/header.vue
+++ app/components/header.vue
@@ -87,7 +87,7 @@ export default {
       navigationItems: [
         { name: "Home", href: "/#home" },
         { name: "Services", href: "/#services" },
-        { name: "Portfolio", href: "/portfolio" },
+        { name: "Portfolio", href: "/#portfolio" },
         { name: "About", href: "/#about" },
         // { name: 'Contact', href: '#contact' }
       ],
